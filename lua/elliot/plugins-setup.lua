7abfdb4a88416887c221611a86dc6335fc9c1afc
nurriol2 <niko.urriola1@gmail.com>
2023-07-11 06:29:14 -0500

    Add autocompletion plugins

--- lua/elliot/plugins-setup.lua
+++ lua/elliot/plugins-setup.lua
@@ -55,6 +55,24 @@ return packer.startup(function(use)
     use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
     use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
+    -- Autocompletion plugins
+    use("hrsh7th/nvim-cmp") -- completion plugin
+    use("hrsh7th/cmp-buffer") -- source for text in buffer
+    use("hrsh7th/cmp-path") -- source for file system paths
+
+    -- Snippets plugins
+    use("L3MON4D3/LuaSnip") -- snippet engine
+    use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+    use("rafamadriz/friendly-snippets") -- useful snippets
+
+    -- -- Manage 
+    -- use("williamboman/mason.nvim")
+    -- -- Configure LSP servers
+    -- use("neovim/nvim-lspconfig")
+    -- -- Bridge the gap b/n mason & lspconfig
+    -- use("williamboman/mason-lspconfig.nvim")
+
+    
     -- Suggested by the Packer docs
     if packer_bootstrap then
         require("packer").sync()
