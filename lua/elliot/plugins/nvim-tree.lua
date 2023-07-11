32e1b52815ef3460e47acc5af15165a32b8f8c60
nurriol2 <niko.urriola1@gmail.com>
2023-07-10 21:20:05 -0500

    Configure nvim-tree according to docs

--- lua/elliot/plugins/nvim-tree.lua
+++ lua/elliot/plugins/nvim-tree.lua
@@ -7,4 +7,29 @@ end
 vim.g.loaded = 1
 vim.g.loaded_netrwPlugin = 1
 
-nvimtree.setup()
+-- Change color for arrows in tree to light blue
+vim.cmd([[ highlight NvimTreeIndentMarker guifg=#3FC5FF ]])
+
+-- Configure nvim-tree
+nvimtree.setup({
+   renderer = {
+    icons = {
+      glyphs = {
+        folder = {
+          arrow_closed = "", -- arrow when folder is closed
+          arrow_open = "", -- arrow when folder is open
+        },
+      },
+    },
+  },   
+  -- disable window_picker for
+  -- explorer to work well with
+  -- window splits
+  actions = {
+    open_file = {
+      window_picker = {
+        enable = false,
+      },
+    },
+  },
+})
