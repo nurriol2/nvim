f28a6c02380c789b2a4fc8c871c8603538470101
nurriol2 <niko.urriola1@gmail.com>
2023-07-12 13:47:47 -0500

    Override colorcolumn highlight

--- lua/elliot/core/colorscheme.lua
+++ lua/elliot/core/colorscheme.lua
@@ -1,4 +1,17 @@
--- Lua initialization file
+-- Lua initialization file --
+
+-- Override cursorcolumn highlight
+-- Moonfly docs recommend putting this before setting the color scheme
+local custom_highlight = vim.api.nvim_create_augroup("CustomHighlight", {})
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "moonfly",
+  callback = function()
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#ae81ff" })
+  end,
+  group = custom_highlight,
+})
+
+-- Set the colorscheme
 local status, _ = pcall(vim.cmd, "colorscheme moonfly")
 if not status then
     print("Colorscheme not found!")
